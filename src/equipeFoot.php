d21db8c99189e4ef54e72b021579b6f038419ade
Thibaut <thib.dhall@gmail.com>
2018-06-25 12:16:06 +0200

    ajout de la classe equipe de foot

---
+++ src/equipeFoot.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+class EquipeFoot
+{
+    private $name;
+    private $vs;
+    private $points;
+
+    public function __construct()
+    {}
+
+    public function     getName()    { return $this->name; }
+    public function     getVs()      { return $this->vs; }
+    public function     getPoints()  { return $this->points; }
+
+
+}
