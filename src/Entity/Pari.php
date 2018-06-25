c99e0e054bcf44c04ace972feef9eed683781705
Thibaut <thib.dhall@gmail.com>
2018-06-25 15:12:43 +0200

    Ajout de l'entite Pari, avec la relation user

---
+++ src/Entity/Pari.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PariRepository")
+ */
+class Pari
+{
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User")
+     */
+    private $user;
+
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $equipe1;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $equipe2;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $score_equipe1;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $score_equipe2;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getEquipe1(): ?string
+    {
+        return $this->equipe1;
+    }
+
+    public function setEquipe1(?string $equipe1): self
+    {
+        $this->equipe1 = $equipe1;
+
+        return $this;
+    }
+
+    public function getEquipe2(): ?string
+    {
+        return $this->equipe2;
+    }
+
+    public function setEquipe2(?string $equipe2): self
+    {
+        $this->equipe2 = $equipe2;
+
+        return $this;
+    }
+
+    public function getScoreEquipe1(): ?int
+    {
+        return $this->score_equipe1;
+    }
+
+    public function setScoreEquipe1(?int $score_equipe1): self
+    {
+        $this->score_equipe1 = $score_equipe1;
+
+        return $this;
+    }
+
+    public function getScoreEquipe2(): ?int
+    {
+        return $this->score_equipe2;
+    }
+
+    public function setScoreEquipe2(?int $score_equipe2): self
+    {
+        $this->score_equipe2 = $score_equipe2;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
---
+++ src/Migrations/Version20180625131133.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180625131133 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE pari (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, equipe1 VARCHAR(255) DEFAULT NULL, equipe2 VARCHAR(255) DEFAULT NULL, score_equipe1 INT DEFAULT NULL, score_equipe2 INT DEFAULT NULL, INDEX IDX_2A091C1FA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE pari ADD CONSTRAINT FK_2A091C1FA76ED395 FOREIGN KEY (user_id) REFERENCES app_users (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE pari');
+    }
+}
