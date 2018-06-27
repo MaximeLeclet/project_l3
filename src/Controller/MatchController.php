201d8357d3c16048b6d0c8e4c6d52dbe29fe8057
Thibaut <thib.dhall@gmail.com>
2018-06-27 14:22:26 +0200

    calculs scores OK

--- src/Controller/MatchController.php
+++ src/Controller/MatchController.php
@@ -21,24 +21,6 @@ class MatchController extends Controller
      */
     function indexAction() {
 
-        //$datas =  json_decode($this->mooc(),true);
-
-        $client = new \GuzzleHttp\Client();
-        $res = $client->request('GET', 'http://daudenthun.fr/api/listing');
-        $datas =  json_decode($res->getBody(),true);
-        $livescore = array();
-        $i = 0;
-        foreach ($datas as $teams){
-            foreach ($teams as $team =>$match){
-                $livescore[$i]['team1'] = $team;
-                $livescore[$i]['team2'] = $match['vs'];
-                $livescore[$i]['score1'] = $match['score'][0];
-                $livescore[$i]['score2'] = $match['score'][1];
-                $livescore[$i]['date'] = \DateTime::createFromFormat('d/m/Y',$match['date']);
-                $livescore[$i]['live'] = $match['live'];
-            }
-            $i++;
-        }
 
         $em = $this->getDoctrine()->getManager();
 
@@ -46,7 +28,46 @@ class MatchController extends Controller
             'user' => $this->getUser()
         ]);
 
-        return $this->render("Matchs/index.html.twig", array('results'=>$livescore, 'userParis'=>$userParis));
+        //$datas =  json_decode($this->mooc(),true);
+        $client = new \GuzzleHttp\Client();
+        $res = $client->request('GET', 'http://http://daudenthun.fr/api/listing');
+        $datas =  json_decode($res->getBody(),true);
+        $livescore = array();
+        $i = 0;
+        foreach ($datas as $teams){
+            foreach ($teams as $team =>$match){
+                $livescore[$i]['team1']             = $team;
+                $livescore[$i]['team2']             = $match['vs'];
+                $livescore[$i]['score1']            = $match['score'][0];
+                $livescore[$i]['score2']            = $match['score'][1];
+                $livescore[$i]['date']              = \DateTime::createFromFormat('d/m/Y',$match['date']);
+                $livescore[$i]['live']              = $match['live'];
+
+                $pointsgagnes = 0;
+                foreach ($userParis as $uP => $onePari)
+                {
+                    if ($onePari->getEquipe1() == $team AND $onePari->getEquipe2() == $match['vs'])
+                    {
+                        if ($onePari->getScoreEquipe1() == $match['score'][0] AND $onePari->getScoreEquipe2() == $match['score'][1])
+                        { $pointsgagnes = 20; }
+                        elseif (($match['score'][0] < $match['score'][1] AND $onePari->getScoreEquipe1() < $onePari->getScoreEquipe2())
+                                OR ($match['score'][0] > $match['score'][1] AND $onePari->getScoreEquipe1() > $onePari->getScoreEquipe2()))
+                        { // On a le bon vainqueur
+                            $pointsgagnes = 5;
+                        }
+                    }
+                }
+                $livescore[$i]['pointsgagnes']      = $pointsgagnes;
+                $this->getUser()->setPoints($this->getUser()->getPoints() + $pointsgagnes);
+            }
+            $i++;
+        }
+
+        return $this->render("Matchs/index.html.twig", array(
+            'user'=>$this->getUser(),
+            'userParis'=>$userParis,
+            'results'=>$livescore
+        ));
     }
 
     static function mooc()
--- src/Controller/PariController.php
+++ src/Controller/PariController.php
@@ -115,8 +115,26 @@ class PariController extends Controller
                 'user' => $this->getUser()
             ]);
 
-            return $this->render('User/index.html.twig', array(
-                'user' => $user,
+            $datas =  json_decode($this->mooc(),true);
+            //$client = new \GuzzleHttp\Client();
+            //$res = $client->request('GET', 'http://http://daudenthun.fr/api/listing');
+            //$datas =  json_decode($res->getBody(),true);
+            $livescore = array();
+            $i = 0;
+            foreach ($datas as $teams){
+                foreach ($teams as $team =>$match){
+                    $livescore[$i]['team1'] = $team;
+                    $livescore[$i]['team2'] = $match['vs'];
+                    $livescore[$i]['score1'] = $match['score'][0];
+                    $livescore[$i]['score2'] = $match['score'][1];
+                    $livescore[$i]['date'] = \DateTime::createFromFormat('d/m/Y',$match['date']);
+                    $livescore[$i]['live'] = $match['live'];
+                }
+                $i++;
+            }
+
+            return $this->render('Matchs/index.html.twig', array(
+                'results'=>$livescore,
                 'paris' => $userParis
             ));
         }
